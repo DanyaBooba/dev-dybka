f3c62f2ec656bda3f07b5a6dd966551f4ba392ef
Daniil Dybka <creagoooff@gmail.com>
2023-06-08 00:27:58 +0300

    save js

--- src/_js/change-theme.js
+++ src/_js/change-theme.js
@@ -1,7 +1,7 @@
 function returnValue() {
 	var val = "";
-	const values = document.querySelectorAll('input[name="color-theme"]');
-	for (const i of values) {
+	var values = document.querySelectorAll('input[name="color-theme"]');
+	for (var i of values) {
 		if (i.checked) {
 			val = i.value;
 		}
@@ -10,24 +10,58 @@ function returnValue() {
 	return val;
 }
 
-function checkValue() {
-	var value = returnValue();
+function setValue(value) {
 	var light = document.getElementById("stylesheetlight");
 	var dark = document.getElementById("stylesheetdark");
+	var values = document.querySelectorAll('input[name="color-theme"]');
+
+	console.log(value);
 
 	if (value === "dark") {
 		localStorage.setItem("color-theme", "dark");
 		light.media = "not all";
 		dark.media = "all";
+
+		for (var i of values) {
+			if (i.value === "dark") {
+				i.setAttribute("checked", "true");
+			} else {
+				i.removeAttribute("checked");
+			}
+		}
 	} else if (value === "light") {
 		localStorage.setItem("color-theme", "light");
 		light.media = "all";
 		dark.media = "not all";
+
+		for (var i of values) {
+			if (i.value === "light") {
+				i.setAttribute("checked", "true");
+			} else {
+				i.removeAttribute("checked");
+			}
+		}
 	} else {
 		localStorage.removeItem("color-theme");
 		light.media = "(prefers-color-scheme: light)";
 		dark.media = "(prefers-color-scheme: dark)";
+
+		for (var i of values) {
+			if (i.value === "auto") {
+				i.setAttribute("checked", "true");
+			} else {
+				i.removeAttribute("checked");
+			}
+		}
 	}
 }
 
-checkValue();
+function checkValue() {
+	setValue(returnValue());
+}
+
+if (localStorage.getItem("color-theme") !== null) {
+	setValue(localStorage.getItem("color-theme"));
+} else {
+	setValue(returnValue());
+}
