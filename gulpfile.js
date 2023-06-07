25b23af9727dcd9eb0a260779223da54bc6de7d5
Daniil Dybka <creagoooff@gmail.com>
2023-06-07 16:04:05 +0300

    add 11ty

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,15 @@
-import gulp from "gulp";
-import htmlmin from "gulp-htmlmin";
-import fileinclude from "gulp-file-include";
-import cssmin from "gulp-cssmin";
-import cssconcat from "gulp-concat-css";
-import autoprefixer from "gulp-autoprefixer";
-import sync from "browser-sync";
+const gulp = require("gulp");
+const htmlmin = require("gulp-htmlmin");
+const cssmin = require("gulp-cssmin");
+const cssconcat = require("gulp-concat-css");
+const autoprefixer = require("gulp-autoprefixer");
+const sync = require("browser-sync");
 
 // HTML
 
-export const html = () => {
+gulp.task("html", () => {
 	return gulp
-		.src("src/html/**/*.html")
-		.pipe(fileinclude())
+		.src("public/**/*.html")
 		.pipe(
 			htmlmin({
 				collapseWhitespace: true,
@@ -19,60 +17,63 @@ export const html = () => {
 			})
 		)
 		.pipe(gulp.dest("dist"));
-};
+});
 
 // Styles:index
 
-export const stylesindex = () => {
+gulp.task("stylesindex", () => {
 	return gulp
-		.src(["src/css/*.css", "src/css/adaptive/*.css"])
+		.src(["src/_css/*.css", "src/_css/adaptive/*.css"])
 		.pipe(autoprefixer())
 		.pipe(cssconcat("index.css"))
 		.pipe(cssmin())
 		.pipe(gulp.dest("dist/css"));
-};
+});
 
 // Styles:const
 
-export const stylesconst = () => {
+gulp.task("stylesconst", () => {
 	return gulp
-		.src("src/css/__const/*.css")
+		.src("src/_css/__const/*.css")
 		.pipe(autoprefixer())
 		.pipe(cssmin())
 		.pipe(gulp.dest("dist/css"));
-};
+});
 
 // JavaScript
 
-export const javascript = () => {
-	return gulp.src("src/js/**/*.js").pipe(gulp.dest("dist/js"));
-};
+gulp.task("javascript", () => {
+	return gulp.src("src/_js/**/*.js").pipe(gulp.dest("dist/js"));
+});
 
 // Media
 
-export const media = () => {
-	return gulp.src("src/media/**/*").pipe(gulp.dest("dist/media"));
-};
+gulp.task("media", () => {
+	return gulp.src("src/_media/**/*").pipe(gulp.dest("dist/img"));
+});
 
 // Fonts
 
-export const fonts = () => {
-	return gulp.src("src/fonts/**/*").pipe(gulp.dest("dist/fonts"));
-};
+gulp.task("fonts", () => {
+	return gulp.src("src/_fonts/**/*").pipe(gulp.dest("dist/fonts"));
+});
 
 // Watch
 
-export const watch = () => {
-	gulp.watch("src/**/*.html", gulp.series(html));
-	gulp.watch("src/**/*.css", gulp.series(stylesindex, stylesconst));
-	gulp.watch("src/**/*.js", gulp.series(javascript));
-	gulp.watch(["src/fonts/**/*", "src/media/**/*"], gulp.series(fonts, media));
-};
+gulp.task("watch", () => {
+	gulp.watch("public/**/*.html", gulp.series("html"));
+	gulp.watch("src/**/*.css", gulp.series("stylesindex", "stylesconst"));
+	gulp.watch("src/**/*.js", gulp.series("javascript"));
+	gulp.watch(
+		["src/_fonts/**/*", "src/_media/**/*"],
+		gulp.series("fonts", "media")
+	);
+});
 
 // Server
 
 sync.create();
-export const server = () => {
+gulp.task("serve", () => {
 	sync.init({
 		ui: false,
 		notify: false,
@@ -80,11 +81,21 @@ export const server = () => {
 			baseDir: "dist",
 		},
 	});
-};
+});
 
 // Default
 
-export default gulp.series(
-	gulp.parallel(html, stylesindex, stylesconst, javascript, fonts, media),
-	gulp.parallel(watch, server)
+gulp.task(
+	"default",
+	gulp.series(
+		gulp.parallel(
+			"html",
+			"stylesindex",
+			"stylesconst",
+			"javascript",
+			"fonts",
+			"media"
+		),
+		gulp.parallel("watch", "serve")
+	)
 );
---
+++ src/index.md
@@ -0,0 +1,32 @@
+---
+layout: "base.njk"
+---
+
+<header class="header">
+    <div class="container">
+        <a href="/" class="main-page" aria-label="Вернуться на главную">
+            <h2>Курсы «Дыбка Даниила»</h2>
+        </a>
+    </div>
+</header>
+
+<main class="container">
+    <div class="row row-cols-1 row-cols-lg-2 gx-5">
+        <div class="col">
+            {% include 'courses/list-courses.njk' %}
+        </div>
+        <div class="col course-empty">
+            <div class="d-flex align-items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" class="me-5"
+                    viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5ZM10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5Z" />
+                </svg>
+                <div>
+                    Выберите<br> интересующий<br> курс слева
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
