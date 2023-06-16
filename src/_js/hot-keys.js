4c1dcd790b1153de21e24595162440561bbc127b
Daniil Dybka <creagoooff@gmail.com>
2023-06-16 21:06:15 +0300

    Add close bottom menu pc

--- src/_js/hot-keys.js
+++ src/_js/hot-keys.js
@@ -22,7 +22,7 @@ function runOnKeys(func, ...codes) {
 function prevPage() {
 	var linkprev = document.getElementById("postprevlesson");
 	if (linkprev !== null) {
-		window.history.replaceState(null, null, linkprev.href + "#");
+		window.history.replaceState(null, null, linkprev.href + "#posttitle");
 		location.reload();
 	}
 }
@@ -30,7 +30,7 @@ function prevPage() {
 function nextPage() {
 	var linknext = document.getElementById("postnextlesson");
 	if (linknext !== null) {
-		window.history.replaceState(null, null, linknext.href + "#");
+		window.history.replaceState(null, null, linknext.href + "#posttitle");
 		location.reload();
 	}
 }
@@ -96,6 +96,40 @@ function checkBottomMobile() {
 	}
 }
 
+function closeBottom() {
+	var bottompc = document.getElementById("bottompc");
+	var buttonpc = document.getElementById("bottompcclosed");
+
+	if (bottompc !== null && buttonpc !== null) {
+		if (bottompc.classList.contains("d-none") === false) {
+			bottompc.classList.add("d-none");
+		}
+
+		if (buttonpc.classList.contains("d-none")) {
+			buttonpc.classList.remove("d-none");
+		}
+
+		localStorage.setItem("bottom-menu-pc", "close");
+	}
+}
+
+function openBottom() {
+	var bottompc = document.getElementById("bottompc");
+	var buttonpc = document.getElementById("bottompcclosed");
+
+	if (bottompc !== null && buttonpc !== null) {
+		if (bottompc.classList.contains("d-none")) {
+			bottompc.classList.remove("d-none");
+		}
+
+		if (buttonpc.classList.contains("d-none") === false) {
+			buttonpc.classList.add("d-none");
+		}
+
+		localStorage.removeItem("bottom-menu-pc");
+	}
+}
+
 // Prev
 
 runOnKeys(
@@ -144,5 +178,11 @@ runOnKeys(
 	"ArrowRight"
 );
 
+// Default
+
 checkBottom();
 checkBottomMobile();
+
+if (localStorage.getItem("bottom-menu-pc") === "close") {
+	closeBottom();
+}
