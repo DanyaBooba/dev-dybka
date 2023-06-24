b4218a02971808d4518fb5d129e8786f7b84afb9
Daniil Dybka <creagoooff@gmail.com>
2023-06-25 00:36:11 +0300

    Get data from url

--- src/_js/private-course.js
+++ src/_js/private-course.js
@@ -1,6 +1,30 @@
 function OpenCourse(course, lesson) {
-	console.log(course + ", " + lesson);
-
 	var content = document.getElementById("postcontent");
-	console.log(content);
+	var codecontent = document.getElementById("privatecoursecode");
+	var placeholder = document.getElementById("privatecourseplaceholder");
+
+	var code = new URL(window.location.href).searchParams.get("c");
+
+	if (code !== null) {
+		codecontent.classList.add("d-none");
+		// placeholder.classList.remove("d-none");
+
+		(function () {
+			var http = new XMLHttpRequest();
+			http.open("GET", "/api/html.txt");
+			http.onreadystatechange = function () {
+				if (this.readyState == 4 && this.status == 200) {
+					var dom = new DOMParser()
+						.parseFromString(this.responseText, "text/html")
+						.getElementsByTagName("body")[0].innerHTML;
+
+					if (dom !== null) {
+						// console.log(dom);
+						content.innerHTML = dom;
+					}
+				}
+			};
+			http.send(null);
+		})();
+	}
 }
