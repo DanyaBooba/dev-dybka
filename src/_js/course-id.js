1c7176c2baba58a3dd7331ed05614d1c36dd11d4
Daniil Dybka <creagoooff@gmail.com>
2023-06-29 15:15:55 +0300

    Update course-id.js

--- src/_js/course-id.js
+++ src/_js/course-id.js
@@ -1,5 +1,20 @@
+function clearText(text) {
+	return text;
+}
+
 function AddID() {
-	console.log("id");
+	var content = document.getElementById("postcontent").children;
+	for (i = 0; i < content.length; i++) {
+		if (
+			content[i].tagName == "H2" ||
+			content[i].tagName == "H3" ||
+			content[i].tagName == "H4" ||
+			content[i].tagName == "H5" ||
+			content[i].tagName == "H6"
+		) {
+			console.log(clearText(content[i].textContent));
+		}
+	}
 }
 
 function AddHash() {
