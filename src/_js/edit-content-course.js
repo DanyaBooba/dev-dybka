d4fe2da72d6213d4314aba56405caaa34cdfa433
Daniil Dybka <creagoooff@gmail.com>
2023-06-29 20:29:25 +0300

    Update edit-content-course.js

--- src/_js/edit-content-course.js
+++ src/_js/edit-content-course.js
@@ -41,28 +41,6 @@ function EditContent() {
 	}
 }
 
-// function EditContent() {
-// 	var codes = document.getElementsByTagName("code");
-// 	for (i = 0; i < codes.length; i++) {
-// 		var code = codes[i];
-// 		if (
-// 			code.classList.contains("language-C#") ||
-// 			code.classList.contains("language-JSON")
-// 		) {
-// 			// console.log(code);
-// 			code.id = "tocopycode" + i;
-
-// 			var div = document.createElement("div");
-// 			div.className = "code-copy";
-// 			div.innerHTML =
-// 				"<button onClick=CopyCode('tocopycode" +
-// 				i +
-// 				"')><svg><use xlink:href='/img/icons/icons.svg#clipboard'></use></svg>Скопировать</button>";
-// 			code.prepend(div);
-// 		}
-// 	}
-// }
-
 function CopyCode(id) {
 	var code = document.getElementById(id);
 	if (code !== null) {
