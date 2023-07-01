d2e7814d1779282fe340bb2c4aab264bf22bd960
Daniil Dybka <creagoooff@gmail.com>
2023-07-01 21:37:03 +0300

    Edit content

--- src/_js/left-bar.js
+++ src/_js/left-bar.js
@@ -1,5 +1,6 @@
 var leftbar = document.getElementById("leftbar").children;
 var buttoninfo = document.getElementById("leftbar_buttoninfo");
+var contentbuttoninfo = document.getElementById("maincontent_buttoninfo");
 var url = window.location.href.split("/");
 
 url.shift();
@@ -9,8 +10,9 @@ url.shift();
 url.shift();
 url = url[0];
 
-if (url === "" && buttoninfo !== null) {
+if (url === "" && buttoninfo !== null && contentbuttoninfo !== null) {
 	buttoninfo.classList.add("active");
+	contentbuttoninfo.classList.add("active");
 }
 
 if (leftbar !== null) {
