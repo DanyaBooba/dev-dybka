fd885f63e424ed9dd4e7a40e745b690825b6e1c4
Daniil Dybka <creagoooff@gmail.com>
2023-07-01 21:13:26 +0300

    Work at pages

--- src/_js/left-bar.js
+++ src/_js/left-bar.js
@@ -1,5 +1,17 @@
 var leftbar = document.getElementById("leftbar").children;
-var url = window.location.href.split("/").at(-2);
+var buttoninfo = document.getElementById("leftbar_buttoninfo");
+var url = window.location.href.split("/");
+
+url.shift();
+url.shift();
+url.shift();
+url.shift();
+url.shift();
+url = url[0];
+
+if (url === "" && buttoninfo !== null) {
+	buttoninfo.classList.add("active");
+}
 
 if (leftbar !== null) {
 	for (i = 0; i < leftbar.length; i++) {
