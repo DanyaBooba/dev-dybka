900423357efa401e6a470c16665ce92b20842123
Daniil Dybka <creagoooff@gmail.com>
2023-09-16 23:47:29 +0300

    Edit links

--- src/_js/course-links.js
+++ src/_js/course-links.js
@@ -1,7 +1,15 @@
 function EditLinks() {
 	var post = document.getElementById("postcontent");
 
-	console.log(post);
+	post.childNodes.forEach((element) => {
+		if (element.tagName == "P") {
+			if (element.childNodes[0].tagName == "A") {
+				var link = element.childNodes[0];
+
+				link.target = "_blank";
+			}
+		}
+	});
 }
 
 EditLinks();
