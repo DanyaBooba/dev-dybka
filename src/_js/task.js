b5325cedc361690578c65a6046c439b11935ed94
Daniil Dybka <creagoooff@gmail.com>
2023-09-21 11:55:10 +0300

    Work at tasks

---
+++ src/_js/task.js
@@ -0,0 +1,110 @@
+// Show Menu
+
+var content = document.getElementById("lessonrightcontent");
+var leftmenu = document.getElementById("lessonleftpcmenu");
+var button = document.getElementById("lessonbuttonmenu");
+
+function hideLeftMenu() {
+	var fullMenu =
+		haveclass(content, "w-100") === true &&
+		haveclass(leftmenu, "d-none") === true &&
+		haveclass(button, "d-none") === false;
+	var shortMenu =
+		haveclass(content, "w-100") === false &&
+		haveclass(leftmenu, "d-none") === false &&
+		haveclass(button, "d-none") === true;
+
+	if (fullMenu) {
+		getShort();
+	} else if (shortMenu) {
+		getFull();
+	} else {
+		defaultMenu();
+	}
+}
+
+function getShort() {
+	localStorage.setItem("left-menu", "short");
+
+	removeclass(content, "w-100");
+	removeclass(leftmenu, "d-none");
+	addclass(button, "d-none");
+}
+
+function getFull() {
+	localStorage.setItem("left-menu", "full");
+
+	addclass(content, "w-100");
+	addclass(leftmenu, "d-none");
+	removeclass(button, "d-none");
+}
+
+function defaultMenu() {
+	localStorage.removeItem("left-menu");
+
+	removeclass(content, "w-100");
+	removeclass(leftmenu, "d-none");
+	addclass(button, "d-none");
+}
+
+function haveclass(item, name) {
+	return item.classList.contains(name);
+}
+
+function addclass(item, name) {
+	item.classList.add(name);
+}
+
+function removeclass(item, name) {
+	item.classList.remove(name);
+}
+
+// Left Bar
+
+function LeftBar() {
+	var leftbar = document.getElementById("leftbar").children;
+	var buttoninfo = document.getElementById("leftbar_buttoninfo");
+	var buttoninfoMobile = document.getElementById("leftbar_buttoninfo_mobile");
+	var buttonfinal = document.getElementById("leftbar_final");
+	var contentbuttoninfo = document.getElementById("maincontent_buttoninfo");
+	var furl = window.location.href.split("/");
+
+	url = furl[5];
+
+	if (
+		url === "" &&
+		buttoninfo !== null &&
+		contentbuttoninfo !== null &&
+		buttoninfoMobile !== null
+	) {
+		buttoninfo.classList.add("active");
+		buttoninfoMobile.classList.add("active");
+		contentbuttoninfo.classList.add("active");
+	}
+
+	if (url === "end" && buttonfinal !== null) {
+		buttonfinal.classList.add("active");
+	}
+
+	if (leftbar !== null) {
+		for (i = 0; i < leftbar.length; i++) {
+			if (leftbar[i].id.slice(8) === url) {
+				leftbar[i].classList.add("active");
+			}
+		}
+	}
+}
+
+// Default
+
+LeftBar();
+
+if (content !== null && leftmenu !== null && button !== null) {
+	var storageLeftMenu = localStorage.getItem("left-menu");
+
+	if (storageLeftMenu === "full") {
+		getFull();
+	} else if (storageLeftMenu === "short") {
+		getShort();
+	}
+}
